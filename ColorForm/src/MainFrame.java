d68745d64e2fd4cf887735e26e2557ebc551af89
Peter Kiraly <peter.kiral@gmail.com>
2023-03-14 18:46:15 +0100

    ColorForm Border színek

--- ColorForm/src/MainFrame.java
+++ ColorForm/src/MainFrame.java
@@ -1,5 +1,18 @@
 import javax.swing.JFrame;
 
+// A panelek miatt:
+import javax.swing.JPanel;
+
+// A BorderLayout miatt:
+import java.awt.BorderLayout;
+
+// GridLayout miatt
+import java.awt.GridLayout;
+
+// setBackground miatt (nem használjuk)
+import java.awt.Color;
+
+
 public class MainFrame extends JFrame{
 
     MainFrame() {
@@ -15,6 +28,41 @@ public class MainFrame extends JFrame{
         this.setSize(500, 400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        // sorok, oszlopok:
+        this.setLayout(new GridLayout(1, 1));
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        // mainPanel.setBackground(Color.PINK);
+        this.add(mainPanel);
+
+        JPanel westPanel = new JPanel();
+        westPanel.setSize(30, 340);
+        westPanel.setBackground(Color.yellow);
+
+        // felrakjuk a mainPanelra és elhelyezzük:
+        mainPanel.add(westPanel, BorderLayout.WEST);
+
+        JPanel northPanel = new JPanel();
+        northPanel.setSize(30, 500);
+        northPanel.setBackground(Color.yellow);
+
+        mainPanel.add(northPanel, BorderLayout.NORTH);
+
+
+        JPanel easatPanel = new JPanel();
+        easatPanel.setSize(30, 340);
+        easatPanel.setBackground(Color.yellow);
+
+        mainPanel.add(easatPanel, BorderLayout.EAST);
+
+        JPanel southPanel = new JPanel();
+        southPanel.setSize(30, 500);
+        southPanel.setBackground(Color.yellow);
+
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+
 
     }
 
