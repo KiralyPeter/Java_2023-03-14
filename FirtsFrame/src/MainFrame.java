c50ad3915c2fabd0f83205e0046b995c2d037735
Peter Kiraly <peter.kiral@gmail.com>
2023-03-14 17:43:23 +0100

    első gomb

--- FirtsFrame/src/MainFrame.java
+++ FirtsFrame/src/MainFrame.java
@@ -1,4 +1,8 @@
+// ebben van az ablak:
 import javax.swing.JFrame;
+
+// ebben meg a gomb:
+import javax.swing.JButton;
 
 public class MainFrame extends JFrame{
 
@@ -18,6 +22,25 @@ public class MainFrame extends JFrame{
 
         // Ablak bezáró gomb, több lehetőség a paraméterben!!
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+
+        // elrendezés miatt kell:
+        this.setLayout(null);
+
+        // hogy az ablak közepén jelenjen meg
+        this.setLocationRelativeTo(null);
+
+
+
+        // *** gomb(ok) *** //
+
+        JButton button = new JButton();
+        button.setText("Gomb");
+        button.setBounds(160, 135, 80, 25);
+        button.setSize(80, 25);
+        
+        
+        // tegyük fel az ablakra:
+        this.add(button);
 
     }
 }
